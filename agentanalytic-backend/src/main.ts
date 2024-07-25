053ac12b02879209805b6e6a8dd9b1bd4a363e8d
Prashant Kumar <prashantnawa123@gmail.com>
2024-07-25 17:33:25 +0530

    production frontend url

--- agentanalytic-backend/src/main.ts
+++ agentanalytic-backend/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const allowedOrigins = ['http://localhost:3001'];
+  const allowedOrigins = ['https://agentanalyticsai.vercel.app/'];
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
