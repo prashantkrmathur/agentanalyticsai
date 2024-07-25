8f7f0286cfd3d18f845a4377111170630a1865fe
Prashant Kumar <prashantnawa123@gmail.com>
2024-07-25 17:17:30 +0530

    config change

--- agentanalytic-backend/src/config/typeorm.ts
+++ agentanalytic-backend/src/config/typeorm.ts
@@ -6,7 +6,8 @@ dotenvConfig({ path: '.env' });
 
 const config = {
     type: 'postgres',
-    host: `${process.env.DATABASE_HOST}`,
+    // host: `${process.env.DATABASE_HOST}`,
+    url :`${process.env.DATABASE_URL}`,
     port: `${process.env.DATABASE_PORT}`,
     username: `${process.env.DATABASE_USERNAME}`,
     password: `${process.env.DATABASE_PASSWORD}`,
