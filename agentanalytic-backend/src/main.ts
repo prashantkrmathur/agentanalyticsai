e71ea278f33ffc69054491ca020a75575e35cf2b
Prashant Kumar <prashantnawa123@gmail.com>
2024-07-26 03:33:37 +0530

    cors issue

--- agentanalytic-backend/src/main.ts
+++ agentanalytic-backend/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const allowedOrigins = ['https://agentanalyticsai.vercel.app/'];
+  const allowedOrigins = ['https://agentanalyticsai.vercel.app', "agentanalyticsai-j4a7gkxmk-prashantkrmathurgmailcoms-projects.vercel.app"];
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
