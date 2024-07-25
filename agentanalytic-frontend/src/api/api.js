570e174791211a67ed66ba89e038124ef85e115c
Prashant Kumar <prashantnawa123@gmail.com>
2024-07-25 17:40:46 +0530

    url

--- agentanalytic-frontend/src/api/api.js
+++ agentanalytic-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://agentanalyticsai-production.up.railway.app/', 
+  baseURL: 'https://agentanalyticsai-production.up.railway.app/api', 
   headers: {
     'Content-Type': 'application/json',
   },
